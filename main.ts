5f638130bb813274a1284f39ccaa6a4af3a14ddc
Andrew <afmcdnl@gmail.com>
2020-10-30 21:21:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (wait_time < Total_time) {
+        if (index < images2.length) {
+            basic.pause(wait_time)
+            images2[index].showImage(0)
+            wait_time += 0.2
+            index += 1
+        } else {
+            index = 0
+        }
+    }
+    radio.setGroup(5)
+    radio.sendValue("Icon2", index)
+    Start()
+})
+function Start () {
+    radio.setGroup(3)
+    wait_time = 0
+    Total_time = randint(3, 6)
+    images2 = [images.iconImage(IconNames.Heart), images.iconImage(IconNames.Umbrella), images.iconImage(IconNames.Giraffe), images.iconImage(IconNames.Duck)]
+}
+let images2: Image[] = []
+let Total_time = 0
+let wait_time = 0
+let index = 0
+index = 0
+Start()
+basic.showIcon(IconNames.Yes)
